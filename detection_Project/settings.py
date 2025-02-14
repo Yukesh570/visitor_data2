73aa6eca5e12da3ee57e5a2435f91900e82298a5
yukesh570 <yukesh.maha10@gmail.com>
2025-02-14 17:01:10 +0545

    whitenoise

--- detection_Project/settings.py
+++ detection_Project/settings.py
@@ -21,7 +21,7 @@ load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
+# See https://docs.djangoproject.ddddddcom/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY =  os.environ.get("SECRET_KEY"),
